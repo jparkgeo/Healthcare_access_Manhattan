d2aeec39f7f79334c568f571902abf0484dcf134
Jinwoo Park <jpark.geo@gmail.com>
2022-08-16 16:24:29 -0500

    update_osmnx.sh to use conda for install/update osmnx package

--- update_osmnx.sh
+++ update_osmnx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # python -m pip install --upgrade osmnx
-python -m pip --upgrade --no-deps --force-reinstall osmnx
-# python -m conda install --channel=conda-forge osmnx --y
+#python -m pip --upgrade --no-deps --force-reinstall osmnx
+python -m conda install --channel=conda-forge osmnx=1.1.2 --y
